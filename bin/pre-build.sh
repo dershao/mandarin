165467d94fef514d9871937fb38cada375277b06
Derek Shao <shao.derek.now@gmail.com>
2024-04-25 23:49:48 -0700

    Load HSK3 module characters only

--- bin/pre-build.sh
+++ bin/pre-build.sh
@@ -8,7 +8,7 @@ fi
 echo "No data found, downloading..."
 
 DATA_FOLDER_NAME=makemeahanzi
-GIT_DOWNLOAD_CHARACTER_DATA="git clone https://github.com/dershao/$DATA_FOLDER_NAME"
+GIT_DOWNLOAD_CHARACTER_DATA="git clone --single-branch --branch hsk3 https://github.com/dershao/$DATA_FOLDER_NAME"
 MV_SVGS_TO_DESTINATION="mv makemeahanzi/svgs src/data/"
 MV_SVGS_STILL_TO_DESTINATION="mv makemeahanzi/svgs-still src/data/"
 MV_CHARACTER_IMAGES_TO_DESTINATION="mv makemeahanzi/character_images src/data/"
